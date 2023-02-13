58f4ad9afb71fbe4c1f8f82a7710334da1532a02
fkiarie <kazungu61@outlook.com>
2023-02-13 19:19:29 +0700

    link sections to nav and add sticky nav
    Linked the sections to the respective nav element
    Made the nav sticky while scrolling

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 document.querySelector('#menu').addEventListener('click',()=>{
     document.querySelector('nav ul').classList.toggle('showmenu')
 })
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = 0;
+
+function myFunction() {
+  sticky = navbar.offsetTop;
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } 
+  else {
+    navbar.classList.remove("sticky");
+  }
+}
